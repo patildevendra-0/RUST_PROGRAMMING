2322e7ad333b6b3885f5334c472e34a201e353af
patildevendra-0 <patildev2001@gmail.com>
2024-01-01 09:01:32 +0530

    new

---
+++ RUST_BASICS.rs
@@ -0,0 +1,42 @@
+
+// Entry Point Function --- main function
+
+/*  
+    ------------------------------------------------------------------------------------------------
+                                        COMPILE RUST PROGRAM
+
+        Compile : 
+                    1.Use rustc     ............. Rust Compiler
+                    2.Use Cargo     ............. Packge Manager
+
+        1) USE RUSTC : 
+                        To Compile Program we use command --
+
+                        rustc Demo.rs
+                        ./Demo
+
+        2) USE CARGO : 
+                        1. For creating new project                        :    cargo new PROJECTNAME
+                        2. Build The Current Project                       :    cargo build
+                        3. Build and Run the current project               :    cargo run
+                        4. Runs the Tests for Current Project              :    cargo test
+                        5. Generate documentation for Current Project      :    cargo doc 
+                        6. Cleaning up compiled files                      :    cargo clean
+                        7. Update Dependencies in Cargo.toml                   :    cargo update
+
+                        
+
+
+
+
+
+
+
+
+    ------------------------------------------------------------------------------------------------
+ */
+
+fn main()
+{
+    println!("SHREE GANESH.....");
+}
